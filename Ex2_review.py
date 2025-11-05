ff557c0e32288a1c25c8af83aa28f1c498c0101f
SHSB2008 <729118@manadulted.ac.uk>
2025-11-05 16:04:25 +0000

    Completed task 4

--- Ex2_review.py
+++ Ex2_review.py
@@ -147,7 +147,9 @@ print("-------------------------------------------\n"
 #
 # Write your code below:
 
-
+number = int(input("Enter a number: "))
+for i in range(1,11):
+    print(f"{number} x {i} = {number * i}")
 # -------------------------------------------
 # Submitting Your Work
 # -------------------------------------------
